c4dd1f739e4e52ae21d0f2fbf2e9ae342434de2c
Dev23504 <devrajsinghsisodiya722gmail.com>
2026-05-08 17:42:38 +0530

    up-to date

--- data/products.js
+++ data/products.js
@@ -1,24 +1,37 @@
 const products = [
+
   ...Array.from({ length: 15 }).map((_, i) => ({
     name: `Men Product ${i + 1}`,
     price: 500 + i * 100,
-    image: `/images/${i + 1}.jpeg`,
-    category: "men"
+    image: `/images/m${i + 1}.jpeg`,
+    category: "men",
+    discount: Math.floor(Math.random() * 40) + 10
   })),
 
   ...Array.from({ length: 15 }).map((_, i) => ({
     name: `Women Product ${i + 1}`,
-    price: 600 + i * 120,
-    image: `/images/w${i + 1}.webp`,
-    category: "women"
+    price: 700 + i * 120,
+    image: `/images/w${i + 1}.jpeg`,
+    category: "women",
+    discount: Math.floor(Math.random() * 40) + 10
   })),
 
   ...Array.from({ length: 10 }).map((_, i) => ({
     name: `Kids Product ${i + 1}`,
     price: 300 + i * 80,
-    image: `/images/k${i + 1}.jpg`,
-    category: "kids"
+    image: `/images/k${i + 1}.jpeg`,
+    category: "kids",
+    discount: Math.floor(Math.random() * 40) + 10
+  })),
+
+  ...Array.from({ length: 10 }).map((_, i) => ({
+    name: `Beauty Product ${i + 1}`,
+    price: 400 + i * 90,
+    image: `/images/b${i + 1}.jpeg`,
+    category: "beauty",
+    discount: Math.floor(Math.random() * 40) + 10
   }))
+
 ];
 
 export default products;
